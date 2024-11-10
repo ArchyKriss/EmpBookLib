b926af3683382746b6c88fbc4aaf03ee3827cfd4
MSI <loreleicruellg@gmail.com>
2024-11-10 19:17:43 +0300

    initial commit

---
+++ src/main/java/com/example/EmployeeBookLib/EmployeeBookLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeBookLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookLibApplication.class, args);
+	}
+
+}
